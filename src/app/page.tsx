7fca0534fced7b4cd30f4d2d973b152c61140e33
Polok Banik <polokbanik7@gmail.com>
2025-06-22 21:46:44 +0600

    adding navbar section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-center">Chai Aur Code</h1>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <h1 className="text-2xl text-center">chai aur code</h1>
+      </main>
     </>
   );
 }
