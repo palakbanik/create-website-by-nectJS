880bce4169a318def853954c0b0e7121ca0b1af2
Polok Banik <polokbanik7@gmail.com>
2025-06-23 00:01:20 +0600

    adding FeatureSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,10 +5,10 @@ import { Button } from "@/components/ui/moving-border";
 export default function HeroSection() {
   return (
     <>
-      <div className="h-auto md:h-[40rem] w-full rounded-full flex flex-col items-center justify-center gap-5 relative overflow-hidden mx-auto py-10 md:py-0 ">
+      <div className="h-auto md:h-screen w-full rounded-full flex flex-col items-center justify-center gap-5 relative overflow-hidden mx-auto py-10 md:py-0 ">
         <div className="p-4 relative z-10 w-full text-center">
           <Spotlight
-            className="-top-50 left-0 md:-top-60 md:left-80"
+            className="-top-70 left-0 md:-top-60 md:left-80"
             fill="white"
           />
           <h1 className=" mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 ">
